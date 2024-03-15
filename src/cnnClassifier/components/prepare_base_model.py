59b5c936233f9697d9c595879d01e914ac840948
dharan1227 <dharanivasan1227@gmail.com>
2024-03-15 22:47:07 +0530

    model training added

--- src/cnnClassifier/components/prepare_base_model.py
+++ src/cnnClassifier/components/prepare_base_model.py
@@ -65,4 +65,5 @@ class PrepareBaseModel:
         
     @staticmethod
     def save_model(path: Path, model: tf.keras.Model):
-        model.save(path)
+        keras_path = str(path.with_suffix('.keras'))
+        model.save(keras_path)
